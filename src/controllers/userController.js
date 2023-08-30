3746440f4cd36f2d6aeaaa0008731fb8fb9aa253
Juliana <julianapereira.dev@gmail.com>
2023-08-29 23:49:48 -0300

    Connection postgres + API is working

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,8 @@
+import {db} from "../database/database.js"
+
 export async function signup(req, res) {
-    res.send("SignUp funcionando")
+  const teste = await db.query(`SELECT * FROM cities`)
+  res.send(teste.rows)
   }
   
   export async function signin(req, res) {
